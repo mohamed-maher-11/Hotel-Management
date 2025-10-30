7565f1dbf02d7fad9f75033d1d491df5db1b5b8d
Taqey <atakieeldeen@gmail.com>
2025-10-30 16:50:21 +0300

    Initial commit - cleaned and ready

--- Backend/HotelManagment.API/Program.cs
+++ Backend/HotelManagment.API/Program.cs
@@ -1,7 +1,12 @@
 
 using HotelManagment.Domain.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.OpenApi.Models;
+using System.Security.Cryptography.Xml;
+using Microsoft.IdentityModel.Tokens;
+using HotelManagment.Application.Implementation.Helper;
+using System.Text;
 namespace HotelManagment.API
 {
 	public class Program
@@ -16,17 +21,62 @@ namespace HotelManagment.API
 			builder.Services.AddControllers();
 			// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 			builder.Services.AddOpenApi();
+			builder.Services.AddSwaggerGen(options =>
+			{
+				options.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Title = "Swagger",
+					Contact = new OpenApiContact
+					{
+						Email = "atakieeldeen@gmail.com",
+						Name = "Taqeyy"
+					}
+				});
+				options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme , new OpenApiSecurityScheme { In=ParameterLocation.Header,Name="Authorization",Scheme=JwtBearerDefaults.AuthenticationScheme});
+				options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference=new OpenApiReference{Type=ReferenceType.SecurityScheme,Id=JwtBearerDefaults.AuthenticationScheme},In=ParameterLocation.Header,Scheme=JwtBearerDefaults.AuthenticationScheme,Name="Authorization"
+						},new List<string>()
+					}
 
+				}); 
+			}
+			);
+			builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+			var Section = builder.Configuration.GetSection("Jwt");
+			var settings = Section.Get<JwtSettings>();
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options=>
+			{
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateAudience = true,
+					ValidateIssuer = true,
+					ValidateIssuerSigningKey = true,
+					ValidateLifetime = true,
+					ValidIssuer = settings.issuer,
+					ValidAudience = settings.audience,
+					ClockSkew = TimeSpan.Zero,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.key))
+				};
+			
+			
+			
+			}
+			);
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.MapOpenApi();
+				app.UseSwagger();
+				app.UseSwaggerUI();
 			}
 
 			app.UseHttpsRedirection();
-
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 
---
+++ Backend/HotelManagment.Application/Implementation/Helper/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelManagment.Application.Implementation.Helper
+{
+	public class JwtSettings
+	{
+		public string key { get; set; }
+		public string issuer { get; set; }
+		public string audience { get; set; }
+
+
+	}
+}
